6b368f2738eab8042bd431e276691ce9960bad06
Siruu123 <112840838+Siruu123@users.noreply.github.com>
2022-09-21 13:38:34 +0530

    new team project added

---
+++ src/main/java/com/springboot/api/SriTejaTeamApi1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriTejaTeamApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriTejaTeamApi1Application.class, args);
+	}
+
+}
